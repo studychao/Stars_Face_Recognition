36de391348bfac8bd7414b85e64db7363556c77a
suyongyu <542882614@qq.com>
2019-12-17 15:01:16 +0800

    添加MYSQL信息

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Please gitignore this file while you actually upload this to the outside world
 
-DB_HOST = ""
-DB_PORT = ""
-DB_USER = ""
-DB_PASSWORD = ""
-DB_NAME = ""
+DB_HOST = "101.132.38.226"
+DB_PORT = "3306"
+DB_USER = "root"
+DB_PASSWORD = "root"
+DB_NAME = "music"
 
 
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
